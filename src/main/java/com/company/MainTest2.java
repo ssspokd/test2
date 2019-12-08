183c0633b00c2e1bed27818c37a9d314a617253e
e.nikolaev.stc <e.nikolaev.stc@innopolis.ru>
2019-12-09 01:09:02 +0300

    first commita

--- src/main/java/com/company/MainTest2.java
+++ src/main/java/com/company/MainTest2.java
@@ -1,13 +1,19 @@
 package com.company;
 
 
+import com.company.dto.PaymentsOfPointSaleDTO;
 import com.company.nio.ReadFile;
 import com.company.nio.WriteToFile;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
 
 public class MainTest2 {
 
@@ -26,7 +32,7 @@ public class MainTest2 {
         }
 	// write your code here
         MainTest2 mainTest2 = new MainTest2();
-        mainTest2.runTask2(args[0],args[2],args[1]);
+        mainTest2.runTask2(args[0],args[1],args[2]);
     }
 
     private void runTask2(String nameFileInput,String nameFileOutputStatsOffice, String nameFileOutputStatsDate ) throws IOException {
@@ -36,11 +42,21 @@ public class MainTest2 {
 
         writeToFile.writeStreamToFile(readFile.getPaymentsOfPointSaleDTO().stream()
                 .collect(Collectors.groupingBy(paymentsOfPointSaleDTO -> paymentsOfPointSaleDTO.getPayment().getDateOperation(),
-                        Collectors.summingDouble(value -> value.getPayment().getSumOperation()))));
+                        Collectors.summingDouble(value -> value.getPayment().getSumOperation())))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new)));
+
+
         writeToFile= new WriteToFile(new File(nameFileOutputStatsDate));
         writeToFile.writeStreamToFile(readFile.getPaymentsOfPointSaleDTO().stream()
                 .collect(Collectors.groupingBy(paymentsOfPointSaleDTO -> paymentsOfPointSaleDTO.getPointOfSale().getIdPointOfSale(),
-                        Collectors.summingDouble(value -> value.getPayment().getSumOperation()))));
+                        Collectors.summingDouble(value -> value.getPayment().getSumOperation())))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new)));
     }
 
 
